093de570ab4260b937883ddf351161745b684fae
gialli24 <a.gialli2409@gmail.com>
2026-03-16 12:15:47 +0100

    update filter by genre functionalty using useEffect

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,32 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function AppMain({ films }) {
 
     const [filteredFilms, setFilteredFilms] = useState(films);
+    const [selectedGenre, setSelectedGenre] = useState("");
 
     const genres = [];
     films.filter(film => {
         if (!genres.includes(film.genre)) {
-            genres.push(film.genre)
+            genres.push(film.genre);
         }
     })
 
-    function filterByGenre(e) {
-        const selectedGenre = e.target.value;
+    useEffect(() => {
 
         if (selectedGenre) {
             setFilteredFilms(
                 films.filter(film => film.genre === selectedGenre)
             )
         } else {
-            setFilteredFilms(films)
+            setFilteredFilms(films);
         }
-    }
+
+    }, [selectedGenre])
 
     return (
         <main>
             <div className="filters">
-                <select onChange={(e) => filterByGenre(e)}>
+                <select onChange={(e) => setSelectedGenre(e.target.value)}>
                     <option value="" >Seleziona un genere</option>
                     {
                         genres.map((genre, i) => (
